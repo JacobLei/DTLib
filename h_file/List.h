90d2a7d7effbc1699654b750c7d486853224ec6e
JacobLei <17763636521@163.com>
2018-07-05 11:00:52 +0800

    set、get添加const属性

--- h_file/List.h
+++ h_file/List.h
@@ -13,8 +13,8 @@ public:
     virtual bool insert(int i, const T& e) = 0;
     virtual bool remove(int i) = 0;
     virtual bool set(int i, const T& e) = 0;
-    virtual bool get(int i, T& e) = 0;
-    virtual int length() = 0;
+    virtual bool get(int i, T& e) const = 0;
+    virtual int length() const = 0;
     virtual void clear() = 0;
 };
 
