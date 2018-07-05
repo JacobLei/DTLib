258288cb0f361ab008764b46fe4248b2fd6b724c
JacobLei <17763636521@163.com>
2018-07-05 14:50:46 +0800

    禁用拷贝构造和赋值操作

--- h_file/List.h
+++ h_file/List.h
@@ -9,7 +9,11 @@ namespace DTLib
 template < typename T >
 class List : public Object
 {
+protected:
+    List(const List&);
+    List& operator= (const List& obj);
 public:
+    List() {}
     virtual bool insert(int i, const T& e) = 0;
     virtual bool remove(int i) = 0;
     virtual bool set(int i, const T& e) = 0;
