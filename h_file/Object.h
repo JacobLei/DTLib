1b25d4107978bd68e3f0ba7866f0a8a16ceba3bb
JacobLei <17763636521@163.com>
2018-07-06 16:35:26 +0800

    添加 == 重载操作符函数

--- h_file/Object.h
+++ h_file/Object.h
@@ -13,6 +13,8 @@ public:
     void operator delete (void* p);
     void* operator new[] (unsigned int size)  throw();
     void operator delete[] (void* p);
+    bool operator == (const Object& obj);
+    bool operator != (const Object& obj);
 
     virtual ~Object() = 0;
 };
@@ -35,6 +37,16 @@ void* Object::operator new[] (unsigned int size) throw()
 void Object::operator delete[] (void* p)
 {
     free(p);
+}
+
+bool Object::operator == (const Object& obj)
+{
+    return this == &obj;
+}
+
+bool Object::operator != (const Object& obj)
+{
+    return this != &obj;
 }
 
 Object::~Object()
