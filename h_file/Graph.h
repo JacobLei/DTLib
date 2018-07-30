09d8e84fd7e11d252f3c5103c32d57090e245258
JacobLei <17763636521@163.com>
2018-07-30 11:11:18 +0800

    添加Graph抽象类

---
+++ h_file/Graph.h
@@ -0,0 +1,37 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "Object.h"
+#include "SharedPointer.h"
+#include "Array.h"
+
+namespace DTLib
+{
+
+template < typename V, typename E >
+class Graph : public Object
+{
+public:
+    virtual V getVertex(int i) = 0;
+    virtual bool getVertex(int i, V& value) = 0;
+    virtual bool setVertex(int i, const V& value) = 0;
+    virtual SharedPointer< Array<int> > getAdjacent(int i) = 0;
+    virtual E getEdge(int i, int j) = 0;
+    virtual bool getEdge(int i, int j, E& value) = 0;
+    virtual bool setEdge(int i, int j, const E& value) = 0;
+    virtual bool removeEdge(int i, int j) = 0;
+    virtual int vCount() = 0;
+    virtual int eCount() = 0;
+    virtual int OD(int i) = 0;
+    virtual int ID(int i) = 0;
+
+    virtual int TD(int i)
+    {
+        return OD(i) + ID(i);
+    }
+
+};
+
+}
+
+#endif // GRAPH_H
